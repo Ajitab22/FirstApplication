8b819c606b1a424bd399bc4644c02122dff8d04b
ajitkumarb <ajitkumarb@STGIDT-NEW-08.stg.com>
2018-03-05 14:13:50 +0530

    Changes

--- SpringRestJpaJsp/src/main/java/com/ajit/SpringRestJpaJsp/SpringRestJpaJspApplication.java
+++ SpringRestJpaJsp/src/main/java/com/ajit/SpringRestJpaJsp/SpringRestJpaJspApplication.java
@@ -2,7 +2,13 @@ package com.ajit.SpringRestJpaJsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan({"/com.*"})
+@EnableJpaRepositories({"/com.*"})
+@EntityScan({"/com.*"})
 @SpringBootApplication
 public class SpringRestJpaJspApplication {
 
