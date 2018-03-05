e373ba6f02f5b479e85b99da8693b14f2cb34095
ajitkumarb <ajitkumarb@STGIDT-NEW-08.stg.com>
2018-03-05 15:06:06 +0530

    Dao,Service,Model Added

--- SpringRestJpaJsp/src/main/java/com/ajit/controller/TopController.java
+++ SpringRestJpaJsp/src/main/java/com/ajit/controller/TopController.java
@@ -1,9 +1,16 @@
 package com.ajit.controller;
 
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("topController")
 public class TopController {
 	
+	@GetMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("index");
+		return "index";
+	}
 }
